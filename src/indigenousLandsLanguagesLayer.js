ca9908126b55dfeff2558a0b110b6cc31f77b6c3
kevinzluo <kevinz.qu@gmail.com>
2018-11-01 23:07:12 -0500

    Added to index.html

--- src/indigenousLandsLanguagesLayer.js
+++ src/indigenousLandsLanguagesLayer.js
@@ -39,33 +39,34 @@ L.LayerGroup.IndigenousLandsLanguagesLayer = L.LayerGroup.extend(
                     var zoom = self._map.getZoom(), origin = self._map.getCenter() ;
                     script.onload = function() {
                         var $ = window.jQuery;
-                        var ILL_url = "https://native-land.ca/api/index.php?maps=languages&position=" + parseInt(origin.lat) + "," + parseInt(origin.lng);
+
+                        //Here is the URL that should be used once we are able to request data.
+                        //var ILL_url = "https://native-land.ca/api/index.php?maps=languages&position=" + parseInt(origin.lat) + "," + parseInt(origin.lng);
+
+                        //Here is the getJSON that will work as well
                         /*$.getJSON(ILL_url , function(data){
                          // console.log(parseInt(origin.lat) +" and "+parseInt(origin.lng)) ;
                          self.parseData(data) ;
                          });*/
-                         var geoData = [{"type":"Feature","properties":{"Name":"Laurentian","description":"https:\/\/native-land.ca\/maps\/languages\/laurentian\/","Slug":"laurentian","FrenchName":"Laurentian","color":"#3A00EE","FrenchDescription":"https:\/\/fr.wikipedia.org\/wiki\/Laurentien_(langue)"},"geometry":{"coordinates":[[[-72.13623,46.027482,0],[-72.949219,45.444717,0],[-73.344727,44.653024,0],[-72.79541,44.386692,0],[-72.180176,44.512176,0],[-71.608887,45.629405,0],[-71.894531,46.255847,0],[-72.13623,46.027482,0]]],"type":"Polygon"},"id":"238225dd9c9c4020f9031d8c80ac9125"},{"type":"Feature","properties":{"Name":"Pennacook","description":"https:\/\/native-land.ca\/maps\/languages\/pennacook\/","Slug":"pennacook","FrenchName":"Pennacook","color":"#022233","FrenchDescription":"https:\/\/native-land.ca\/maps\/languages\/pennacook\/"},"geometry":{"coordinates":[[[-72.04834,45.151053,5],[-72.312012,44.746733,5],[-72.79541,44.197959,5],[-72.312012,43.100983,0],[-71.938477,42.585444,0],[-71.103516,42.5207,0],[-70.488281,42.55308,0],[-70.202637,43.229195,0],[-70.708008,43.786958,0],[-71.081543,44.840291,0],[-71.169434,45.429299,0],[-71.938477,45.197522,0],[-72.04834,45.151053,0]]],"type":"Polygon"},"id":"664f06138da4f296820b2867e0ba31ae"}];
 
-                         /*function onEachFeature(feature, layer) {
-                           layer.bindPopup("<strong>Name: </strong>" + feature.properties.Name + "<br><strong>Description: </strong> <a href=" + feature.properties.description + ">Native Lands - " + feature.properties.Name + "</a>" );
+                        //This is the sample set of data I used to test
+                        //var data = [{"type":"Feature","properties":{"Name":"Laurentian","description":"https:\/\/native-land.ca\/maps\/languages\/laurentian\/","Slug":"laurentian","FrenchName":"Laurentian","color":"#3A00EE","FrenchDescription":"https:\/\/fr.wikipedia.org\/wiki\/Laurentien_(langue)"},"geometry":{"coordinates":[[[-72.13623,46.027482,0],[-72.949219,45.444717,0],[-73.344727,44.653024,0],[-72.79541,44.386692,0],[-72.180176,44.512176,0],[-71.608887,45.629405,0],[-71.894531,46.255847,0],[-72.13623,46.027482,0]]],"type":"Polygon"},"id":"238225dd9c9c4020f9031d8c80ac9125"},{"type":"Feature","properties":{"Name":"Pennacook","description":"https:\/\/native-land.ca\/maps\/languages\/pennacook\/","Slug":"pennacook","FrenchName":"Pennacook","color":"#022233","FrenchDescription":"https:\/\/native-land.ca\/maps\/languages\/pennacook\/"},"geometry":{"coordinates":[[[-72.04834,45.151053,5],[-72.312012,44.746733,5],[-72.79541,44.197959,5],[-72.312012,43.100983,0],[-71.938477,42.585444,0],[-71.103516,42.5207,0],[-70.488281,42.55308,0],[-70.202637,43.229195,0],[-70.708008,43.786958,0],[-71.081543,44.840291,0],[-71.169434,45.429299,0],[-71.938477,45.197522,0],[-72.04834,45.151053,0]]],"type":"Polygon"},"id":"664f06138da4f296820b2867e0ba31ae"}];
+                        //self.parseData(data);
+
+                        /*Here is a much simpler way to add the layer using geoJSON, because the data is already in geoJSON format
+                        This does all that parseData does in a much simpler format.
+                        function onEachFeature(feature, layer) {
+                         layer.bindPopup("<strong>Name : </strong>" + nme + "<br><strong>Description: </strong> <a href=" + desc + ">Native Lands - " + nme + "</a><br><i>From the  (<a href='https://publiclab.org/notes/sagarpreet/06-06-2018/leaflet-environmental-layer-library?_=1528283515'>info<a>)</i>");
+                        }
+
+                        function getStyle(feature, layer) {
+                         return {
+                           "color": feature.properties.color
                          }
+                        }
 
-                         function getStyle(feature, layer) {
-                           return {
-                             "color": feature.properties.color
-                           }
-                         }
-
-                         self.addLayer(L.geoJSON(geoData, {style: getStyle, onEachFeature: onEachFeature}));
-                         */
-                         self.parseData(data);
-
-                         //self.addLayer(L.geoJSON(geoData));
-
-                         console.log(self);
-
-
-
+                        self.addLayer(L.geoJSON(geoData, {style: getStyle, onEachFeature: onEachFeature}));
+                        */
                     };
                     document.getElementsByTagName("head")[0].appendChild(script);
                 })();
@@ -77,20 +78,13 @@ L.LayerGroup.IndigenousLandsLanguagesLayer = L.LayerGroup.extend(
         getPoly: function (data) {
               var coords = data.geometry.coordinates;
 
-
-              console.log("coords");
-              console.log(coords[0]);
-
-              console.log(coords[0][0][1]);
-
+              //Because geoJSON has coordinates in lng, lat format, we must reverse them
               for( j = 0; j < coords[0].length; j++) {
                 var temp = coords[0][j][1];
                 coords[0][j][1] = coords[0][j][0];
                 coords[0][j][0] = temp;
               }
 
-              console.log("should be flipped");
-              console.log(coords[0]);
 
               var nme = data.properties.Name;
               var frNme = data.properties.FrenchName;
@@ -99,9 +93,9 @@ L.LayerGroup.IndigenousLandsLanguagesLayer = L.LayerGroup.extend(
               var clr = data.properties.color;
               var ill_poly ;
               if (!isNaN((coords[0][0][0]) && !isNaN((coords[0][0][1]))) ){
-                //console.log("Made it into getPoly");
+
               ill_poly = L.polygon(coords, {color: clr}).bindPopup("<strong>Name : </strong>" + nme + "<br><strong>Description: </strong> <a href=" + desc + ">Native Lands - " + nme + "</a><br><i>From the  (<a href='https://publiclab.org/notes/sagarpreet/06-06-2018/leaflet-environmental-layer-library?_=1528283515'>info<a>)</i>") ;
-                //console.log("properly created polygon");
+
               }
             return ill_poly ;
         },
@@ -112,20 +106,20 @@ L.LayerGroup.IndigenousLandsLanguagesLayer = L.LayerGroup.extend(
             console.log(poly);
             if (!this._layers[key]) {
                 this._layers[key] = poly;
-                //console.log("about to add");
+
                 this.addLayer(poly);
                 console.log("logging layer");
                 console.log(this);
-                //console.log("should have added polygon layer");
+
             }
         },
 
         parseData: function (data) {
-        //console.log(data);
+
         if (!!data){
            for (i = 0 ; i < data.length ; i++) {
             console.log("made it into loop");
-            //console.log(data[i]);
+
             this.addPoly(data[i]) ;
             console.log("finished iter of loop");
 
