19b5bbb6acc021cdc716eb94904bce4b1ea1245f
kevinzluo <kevinz.qu@gmail.com>
2018-11-04 20:51:02 -0600

    Added marker creation per individual station.

--- src/aqicnLayer.js
+++ src/aqicnLayer.js
@@ -4,6 +4,7 @@ L.LayerGroup.AQICNLayer = L.LayerGroup.extend(
         options: {
             popupOnMouseover: true,
             clearOutsideBounds: false,
+            tokenID: "566331c289f0aeacd78e0b18362b4bcfa5097572"
         },
         
         initialize: function (options) {
@@ -26,8 +27,6 @@ L.LayerGroup.AQICNLayer = L.LayerGroup.extend(
         
         requestRegionData: function () {
                 var self = this ; 
-                var tokenID = "566331c289f0aeacd78e0b18362b4bcfa5097572";
-
                 
                 (function() {
                     var script = document.createElement("SCRIPT");
@@ -38,18 +37,64 @@ L.LayerGroup.AQICNLayer = L.LayerGroup.extend(
                     
                     script.onload = function() {
                         var $ = window.jQuery;
-                        var AQI_url = "https://api.waqi.info/map/bounds/?latlng=" + southwest.lat + "," + southwest.lng + "," + northeast.lat + "," + northeast.lng + "&token=" + tokenID;
+                        var AQI_url = "https://api.waqi.info/map/bounds/?latlng=" + southwest.lat + "," + southwest.lng + "," + northeast.lat + "," + northeast.lng + "&token=" + self.options.tokenID;
                         console.log(AQI_url);
                         
-                         $.getJSON(AQI_url , function(data){
+                         $.getJSON(AQI_url , function(regionalData){
                              // console.log(parseInt(origin.lat) +" and "+parseInt(origin.lng)) ;
-                             console.log(data);
-                             //self.parseData(data) ;    
+                             console.log(regionalData);
+                             self.parseData(regionalData) ;    
                          });
                     };
                     document.getElementsByTagName("head")[0].appendChild(script);
                 })();             
-        },    
+        }, 
+        
+        requestStationData: function(stationID) {
+
+            
+            console.log("Trying to request data here");
+
+            if(!this._layers[stationID]) { //catch here to prevent extra parsing
+                var self = this;
+                var $ = window.jQuery;
+                
+                var AQI_station_url = "https://api.waqi.info/feed/@" + stationID + "/?token=" + this.options.tokenID;
+                console.log(AQI_station_url);
+                $.getJSON(AQI_station_url, function(data) {
+                    console.log("Fetched Station Data:");
+                    console.log(data);
+                    self.addMarker(data);
+                }); 
+            }
+        },
+        
+        addMarker: function(stationData) {            
+            var aqi = stationData.data.aqi;
+            var latlng = stationData.data.city.geo;
+            var name = stationData.data.city.name;
+            var url = stationData.data.url;
+            var key = stationData.data.idx;
+            var AQI_marker = L.marker(latlng).bindPopup(name);
+            this._layers[key] = AQI_marker;
+            this.addLayer(AQI_marker);
+            
+        },
+        
+        parseData: function(regionalData) {
+            if(!!regionalData) {
+                console.log("made line 90");
+                for(var i = 0; i < regionalData.data.length; i++) {
+                    //console.log(regionalData.data[i]);
+                    this.requestStationData(regionalData.data[i].uid);
+                }
+                
+                if(this.options.clearOutsideBounds) {
+                    this.clearOutsideBounds();
+                }
+            }
+            
+        }
     }
 );
 
